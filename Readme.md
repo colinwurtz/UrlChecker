2b225636e6d1d2bf3a6c773476a6997fd9434c8a
Colin Wurtz <cwurtz@valorem.com>
2020-09-02 12:05:41 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# UrlChecker
+
+- Put Urls.csv in same folder as UrlChecker.ps1
+- Modidy Urls.csv with short links and expected link output
+- Double click UrlChecker.ps1 or run through Powershell
+- Results will be written to Results.csv in the same folder where this is being executed
---
+++ UrlChecker.ps1
@@ -0,0 +1,28 @@
+﻿$csv = Import-Csv "$PSScriptRoot\Urls.csv"
+$outFile = "$PSScriptRoot\Results.csv"
+
+'"ShortLink","Redirect","Result"' | Out-File -FilePath $outFile -Append
+$lines = @()
+$csv| ForEach-Object {
+    
+    Write-Host "Checking if" $_.ShortLink "redirects to:" $_.Redirect
+    $line = $_
+
+    $Response = Invoke-WebRequest -URI $_.ShortLink -UseBasicParsing
+    Write-Host $Response.BaseResponse.ResponseUri
+    
+    if ($Response.BaseResponse.ResponseUri –eq $_.Redirect) {
+         $result = "Works"
+    } else {
+         $result = "Failed"
+    }
+
+    Write-Host $line
+    $line | Add-Member -NotePropertyName Result -NotePropertyValue $result
+    $lines += $line
+     
+}
+
+$lines | Export-Csv -Path $outFile -NoTypeInformation
+
+# $_.ShortLink + ","  + $_.Redirect + "," + $result | Out-File -FilePath $outFile -Append
